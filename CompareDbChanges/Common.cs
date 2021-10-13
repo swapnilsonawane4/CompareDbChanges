63ff109bb2a805a556c58785b228a2973ca65a29
swapnil sonawane <swapnil.sonawane@excellarate.com>
2021-10-13 15:57:20 +0530

    Add project files.

---
+++ CompareDbChanges/Common.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace CompareDbChanges
+{
+    class Common
+    {
+        public static List<string> getTableList(string connString)
+        {
+            string cmd = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+
+            DataTable dt = new DataTable();
+            dt = getDataTable(connString, cmd);
+            List<string> tableName = new List<string>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                tableName.Add(dt.Rows[i][2].ToString());
+            }
+            return tableName;
+        }
+        public static DataTable getDataTable(string connectionString, string sqlCommand)
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(connectionString);
+                SqlCommand cm = new SqlCommand(sqlCommand, con);
+                con.Open();
+                SqlDataReader sdr = cm.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(sdr);
+
+                return dt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public static void Write(string path, string firstParam, string secondParam)
+        {
+            using (StreamWriter writer = new FileInfo(path).AppendText())
+            {
+                writer.Write(firstParam, secondParam);
+            }
+        }
+    }
+}
---
+++ CompareDbChanges/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CompareDbChanges
+{
+    class Program
+    {
+        readonly static string fullPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"tableData.txt");
+        readonly static string resultPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Result.txt");
+        
+        static void Main(string[] args)
+        {
+            List<string> tableName = new List<string>();
+            
+            string sourceConnString = "server=.;database=FM_Master;Integrated Security=True;MultipleActiveResultSets=true;";
+            string destConnString = "server=.;database=FM_Hti_Local;Integrated Security=True;MultipleActiveResultSets=true;";
+            
+            tableName = Common.getTableList(sourceConnString); 
+            createTextFile(tableName,sourceConnString, fullPath);
+            compare(destConnString, fullPath, resultPath);
+        }
+
+        public static void checkColumns(string tableName,string columnName, string dataType, string connString,string resultPath)
+        {
+            //connection get columnName,dtype
+            string cmd = "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '" + tableName + "'";
+            DataTable dt = new DataTable();
+            dt = Common.getDataTable(connString, cmd);
+
+            int flag = 0;
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {   
+                if (columnName == dt.Rows[i][3].ToString())
+                {
+                    //present column
+                    Common.Write(resultPath, "Present Columns - {0}.\n", columnName);
+                    if (dataType == dt.Rows[i][7].ToString())
+                    {
+                        //present datatype
+                        Common.Write(resultPath, "Present DataType - {0}.\n", dataType);
+                        flag = 1;
+                    }
+                    else
+                    {
+                        //not present datatype
+                        Common.Write(resultPath, "Not Present DataType - {0}.\n", dataType);
+                    }
+                }
+                
+            }
+            if(flag == 0)
+            {
+                //not present column
+                Common.Write(resultPath, "Not Present Columns - {0}.\n", columnName);
+            }
+        }
+        public static void compare(string destConnString,string fullPath,string resultPath)
+        {
+            File.WriteAllText(resultPath, "");
+            string readText = File.ReadAllText(fullPath);
+
+            //get data from sql
+            List<string> tableName = new List<string>();
+            tableName = Common.getTableList(destConnString);
+
+            //compare
+            string[] data = readText.Split("||");
+            var tbname = "";
+            var cname = "";
+            var dataType = "";
+            foreach (string d in data)
+            {
+                string[] data2 = d.Split("|");
+                foreach (string k in data2)
+                {
+                    string[] data3 = k.Split(",");
+                    if ((data3 != null) && (data3[0] != ""))
+                    {
+                        tbname = data3[1];
+                    }
+                }
+                var result = tableName.FirstOrDefault(x => x == tbname);
+                if (result != null)
+                {
+                    Common.Write(resultPath, "Present Tables - {0}.\n", tbname);
+                    foreach (string k in data2)
+                    {
+                        string[] data3 = k.Split(",");
+                        if ((data3 != null) && (data3[0] != ""))
+                        {
+                            tbname = data3[1];
+                            cname = data3[2];
+                            dataType = data3[3];
+                            
+                        }
+                        checkColumns(tbname, cname, dataType, destConnString,resultPath);
+
+                    }
+                    
+
+                }
+                else
+                {
+                    Common.Write(resultPath, "Not Present Tables - {0}.\n", tbname);
+                }
+
+            }
+            //log
+
+        }
+        public static void createTextFile(List<string> tableName,string connString,string fullPath)
+        {
+            File.WriteAllText(fullPath, "");
+            foreach (var tName in tableName)
+            {
+                string cmd = "select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '" + tName + "'";
+                DataTable dt = new DataTable();
+                dt = Common.getDataTable(connString, cmd);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    using (StreamWriter writer = new FileInfo(fullPath).AppendText())
+                    {
+                        writer.Write("{0},{1},{2},{3}|", dt.Rows[i][0].ToString(), tName,dt.Rows[i][3].ToString(), dt.Rows[i][7].ToString());
+                    }
+                }
+                Common.Write(fullPath, "|", null);
+            }
+        }
+        
+    }
+}
